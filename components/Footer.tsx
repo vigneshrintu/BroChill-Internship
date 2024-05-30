5c4f1e6ccaafcfd2f12efb5d636ac08a0d6de776
Vigneshrintu <vigneshrintu@gmail.com>
2024-05-30 17:04:10 +0530

    fixed production build issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
@@ -20,7 +20,7 @@ const Footer = () => {
           Address
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-        Third Floor, Ravi Chambers, Kavuri Hills, Madhapur, Telangana 500083.
+          Third Floor, Ravi Chambers, Kavuri Hills, Madhapur, Telangana 500083.
         </p>
         <Link href="mailto:contact@brochill.com">
           <MagicButton
@@ -37,13 +37,12 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <Link href={info.url}>
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
+            <Link key={info.id} href={info.url}>
+              <div
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </div>
             </Link>
           ))}
         </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,18 @@
+"use client";
+import React from "react";
 import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-
+import { WavyBackground } from "../components/ui/wavy-background";
 const Hero = () => {
   return (
-    <div id="home" className="pb-20 pt-36">
+    <div id="home" className="pb-4 pt-4">
       
-      <div>
+      {/* <div>
        
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="green" />
-      </div>
+      </div> */}
 
       
       <div
@@ -25,8 +27,8 @@ const Hero = () => {
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
-
-      <div className="flex justify-center relative my-20 z-10">
+      <WavyBackground className="max-w-4xl mx-auto ">
+      <div className="flex justify-center relative mt-72 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           {/* <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             local is the new global
@@ -49,8 +51,10 @@ const Hero = () => {
               position="right"
             />
           </a>
+
         </div>
       </div>
+        </WavyBackground>
     </div>
   );
 };
