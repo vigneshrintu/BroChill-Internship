69939780c63eeb3b03def1e484300e17c49e0091
Vigneshrintu <vigneshrintu@gmail.com>
2024-07-16 13:02:32 +0530

    commented signin signup options in mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -75,8 +75,8 @@ function Navbar({ className }: { className?: string }) {
           <Link href="/#home">Home</Link>
           <Link href="/#about">About</Link>
           <Link href="/#contact">Contact</Link>
-          <Link href='/sign-up'>Sign up</Link>
-          <Link href='/sign-in'>Sign In</Link>
+          {/* <Link href='/sign-up'>Sign up</Link>
+          <Link href='/sign-in'>Sign In</Link> */}
           <Link href="/privacy-policy">Privacy Policy</Link>
           <Link href="/terms-of-service">Terms of Service</Link>
           <Link href="/content-community-guidelines">Content & Community Guidelines</Link>
